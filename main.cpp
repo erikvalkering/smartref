4eb75ac7e5208c0285f4e0d1647561b3f2c1aedf
Anita Laera <al@plaxis.com>
2017-07-01 16:38:32 +0200

    Added first (intrusive) version for forwarding member functions

---
+++ main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <vector>
+
+#define MEMBER(member)                                                                               \
+    template<typename... T>                                                                          \
+    auto member(T &&... args) -> decltype(std::declval<Delegate>().member(std::forward<T>(args)...)) \
+    {                                                                                                \
+        return delegate().member(std::forward<T>(args)...);                                          \
+    }                                                                                                \
+
+template<class Derived, typename Delegate>
+class using_
+{
+private:
+    decltype(auto) delegate()
+    {
+        //! Downcast to the derived class
+        auto &derived = static_cast<Derived &>(*this);
+
+        //! Now invoke the conversion operator
+        return static_cast<Delegate &>(derived);
+    }
+
+public:
+    MEMBER(push_back)
+    MEMBER(begin)
+    MEMBER(end)
+};
+
+template<typename T>
+class MyVector : public using_<MyVector<T>, std::vector<T>>
+{
+public:
+    operator std::vector<T> &()
+    {
+        return data;
+    }
+
+    std::vector<T> data;
+};
+
+template<typename NumericalType>
+class Number
+{
+public:
+    operator NumericalType &()
+    {
+        return data;
+    }
+
+    NumericalType data;
+};
+
+int main()
+{
+    std::cout << "Hello, Wandbox!" << std::endl;
+
+    MyVector<int> v;
+
+    v.push_back(1);
+    v.push_back(2);
+    v.push_back(3);
+    //v.push_back(1, 2, 3);
+
+    for (auto x : v)
+    {
+        std::cout << x << std::endl;
+    }
+
+    Number<int> x{};
+    Number<int> y{};
+
+    auto z = x + y;
+
+    std::cout << z << std::endl;
+    std::cout << typeid(z).name() << std::endl;
+}
