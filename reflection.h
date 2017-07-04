6e4f47660ac7e8a69d91604991f1706bfb3b00e9
Anita Laera <al@plaxis.com>
2017-07-04 23:52:29 +0200

    Implemented reflected_member_count_v with unit test for zero reflected members.

--- reflection.h
+++ reflection.h
@@ -23,11 +23,24 @@ struct reflected_member
 template<typename T, size_t counter>
 using reflected_member_t = typename reflected_member<T, counter>::type;
 
-// template<typename T>
-// constexpr auto reflected_member_count_v =;
+template<typename T, size_t count = 0, typename = reflected_member_t<T, count>>
+struct reflected_member_count
+{
+    static constexpr auto value = reflected_member_count<T, count + 1>::value;
+};
+
+template<typename T, size_t count>
+struct reflected_member_count<T, count, void>
+{
+    static constexpr auto value = count;
+};
+
+template<typename T>
+constexpr auto reflected_member_count_v = reflected_member_count<T>::value;
 
 struct Foo {};
 static_assert(std::is_same_v<void, reflected_member_t<Foo, 0>>);
+static_assert(reflected_member_count_v<Foo> == 0);
 
 struct Bar {};
 template<> struct reflected_member<Bar, CURRENT_COUNTER(Bar)> {using type = int;}; INC_COUNTER(Bar);
