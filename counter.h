b1ac274c6135145a26362dfab32d9c544828f067
Anita Laera <al@plaxis.com>
2017-07-04 23:32:10 +0200

    Generalized the counter to be type-specific.

--- counter.h
+++ counter.h
@@ -2,35 +2,36 @@
 
 //! Compile-time counter. Credits go to the folks at CopperSpice (see CppCon2015 talk).
 
-template<size_t count>
-struct Counter : Counter<count - 1>
+template<typename T, size_t count>
+struct Counter : Counter<T, count - 1>
 {
     static constexpr auto value = count;
 };
 
-template<>
-struct Counter<0>
+template<typename T>
+struct Counter<T, 0>
 {
     static constexpr auto value = 0;
 };
 
-Counter<0> counter(Counter<0>);
+template<typename T>
+Counter<T, 0> counter(Counter<T, 0>);
 
 #define TOKENPASTE2(x, y) x ## y
 #define TOKENPASTE(x, y) TOKENPASTE2(x, y)
 
-#define CURRENT_COUNTER()                    \
-    decltype(counter(Counter<255>{}))::value \
+#define CURRENT_COUNTER(T)                    \
+    decltype(counter(Counter<T, 255>{}))::value \
 
-#define INC_COUNTER()                                                                            \
-    constexpr auto TOKENPASTE(value_, __LINE__) = CURRENT_COUNTER();                             \
-    Counter<TOKENPASTE(value_, __LINE__) + 1> counter(Counter<TOKENPASTE(value_, __LINE__) + 1>) \
+#define INC_COUNTER(T)                                                                                 \
+    constexpr auto TOKENPASTE(value_, __LINE__) = CURRENT_COUNTER(T);                                  \
+    Counter<T, TOKENPASTE(value_, __LINE__) + 1> counter(Counter<T, TOKENPASTE(value_, __LINE__) + 1>) \
 
-static_assert(CURRENT_COUNTER() == 0);
-static_assert(CURRENT_COUNTER() == 0);
-INC_COUNTER();
-static_assert(CURRENT_COUNTER() == 1);
-static_assert(CURRENT_COUNTER() == 1);
-INC_COUNTER();
-static_assert(CURRENT_COUNTER() == 2);
-static_assert(CURRENT_COUNTER() == 2);
+static_assert(CURRENT_COUNTER(void) == 0);
+static_assert(CURRENT_COUNTER(void) == 0);
+INC_COUNTER(void);
+static_assert(CURRENT_COUNTER(void) == 1);
+static_assert(CURRENT_COUNTER(void) == 1);
+INC_COUNTER(void);
+static_assert(CURRENT_COUNTER(void) == 2);
+static_assert(CURRENT_COUNTER(void) == 2);
