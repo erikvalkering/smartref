6caee9d74c97644b09cfbb331200580321a63f9a
Anita Laera <al@plaxis.com>
2017-07-05 21:43:48 +0200

    Added some classes to foo.h

--- foo.h
+++ foo.h
@@ -1,17 +1,36 @@
 #pragma once
 
 #include "using.h"
+#include "reflection.h"
 
 struct Foo
 {
-    void bar()
+    void foo()
     {
-        std::cout << "Foo::bar" << std::endl;
+        std::cout << "Foo::foo" << std::endl;
     }
 };
 
 template<typename Derived>
 struct MemberFunctions<Derived, Foo>
 {
-    MEMBER(bar)
+    MEMBER(foo)
+};
+
+struct Bar
+{
+    void bar()
+    {
+        std::cout << "Bar::bar" << std::endl;
+    }
+};
+
+REFLECT_MEMBER(Bar, bar);
+
+struct Baz
+{
+    void REFLECT(baz)()
+    {
+        std::cout << "Baz::baz" << std::endl;
+    }
 };
