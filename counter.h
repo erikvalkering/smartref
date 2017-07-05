6bcdc719c4d4f50acc09af2363f28a44df4bfd36
Anita Laera <al@plaxis.com>
2017-07-06 00:04:25 +0200

    Several fixes in counter.h after adding a namespace to it

--- counter.h
+++ counter.h
@@ -17,17 +17,21 @@ struct Counter<T, 0>
 };
 
 template<typename T>
-Counter<T, 0> counter(Counter<T, 0>);
+Counter<T, 0> __counter(Counter<T, 0>);
+
+} // namespace counter
 
 #define TOKENPASTE2(x, y) x ## y
 #define TOKENPASTE(x, y) TOKENPASTE2(x, y)
 
-#define CURRENT_COUNTER(T)                    \
-    decltype(counter(Counter<T, 255>{}))::value \
+#define CURRENT_COUNTER(T)                                  \
+    decltype(__counter(counter::Counter<T, 255>{}))::value  \
 
-#define INC_COUNTER(T)                                                                                 \
-    constexpr auto TOKENPASTE(value_, __LINE__) = CURRENT_COUNTER(T);                                  \
-    Counter<T, TOKENPASTE(value_, __LINE__) + 1> counter(Counter<T, TOKENPASTE(value_, __LINE__) + 1>) \
+#define INC_COUNTER(T)                                                                                      \
+    namespace counter {                                                                                     \
+    constexpr auto TOKENPASTE(value_, __LINE__) = CURRENT_COUNTER(T);                                       \
+    Counter<T, TOKENPASTE(value_, __LINE__) + 1> __counter(Counter<T, TOKENPASTE(value_, __LINE__) + 1>);   \
+    } /* namespace counter */                                                                               \
 
 static_assert(CURRENT_COUNTER(void) == 0);
 static_assert(CURRENT_COUNTER(void) == 0);
@@ -37,5 +41,3 @@ static_assert(CURRENT_COUNTER(void) == 1);
 INC_COUNTER(void);
 static_assert(CURRENT_COUNTER(void) == 2);
 static_assert(CURRENT_COUNTER(void) == 2);
-
-} // namespace counter
