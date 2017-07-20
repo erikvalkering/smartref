e0a7d58ea2c5789ee56136470c064fafcc61ff2b
Erik Valkering <erikvalkering@gmail.com>
2017-07-20 23:49:05 +0200

    Moved testing code of reflection into separate cpp

---
+++ reflection.cpp
@@ -0,0 +1,56 @@
+#include "reflection.h"
+
+struct Foo {};
+static_assert(std::is_same_v<void, reflection::reflected_member_t<Foo, 0>>);
+static_assert(reflection::reflected_member_count_v<Foo> == 0);
+
+struct Bar
+{
+    void bar();
+};
+REFLECT_MEMBER(Bar, bar);
+static_assert(!std::is_same_v<void, reflection::reflected_member_t<Bar, 0>>);
+static_assert( std::is_same_v<void, reflection::reflected_member_t<Bar, 1>>);
+static_assert(reflection::reflected_member_count_v<Bar> == 1);
+
+struct Baz
+{
+    void baz();
+    void baz2();
+};
+REFLECT_MEMBER(Baz, baz);
+REFLECT_MEMBER(Baz, baz2);
+static_assert(!std::is_same_v<void, reflection::reflected_member_t<Baz, 0>>);
+static_assert(!std::is_same_v<void, reflection::reflected_member_t<Baz, 1>>);
+static_assert( std::is_same_v<void, reflection::reflected_member_t<Baz, 2>>);
+static_assert(reflection::reflected_member_count_v<Baz> == 2);
+
+struct Bat
+{
+    void REFLECT(bat)();
+    void REFLECT(bat2)();
+};
+
+static_assert(!std::is_same_v<void, reflection::reflected_class_member_t<Bat, 0>>);
+static_assert(!std::is_same_v<void, reflection::reflected_class_member_t<Bat, 1>>);
+static_assert( std::is_same_v<void, reflection::reflected_class_member_t<Bat, 2>>);
+static_assert(reflection::reflected_class_member_count_v<Bat> == 2);
+
+auto _ = []{
+    using namespace reflection;
+
+    std::cout << "Foo" << std::endl;
+    std::cout << reflected_member_count_v<Foo> << std::endl;
+
+    std::cout << "Bar" << std::endl;
+    std::cout << reflected_member_count_v<Bar> << std::endl;
+
+    std::cout << "Baz" << std::endl;
+    std::cout << reflected_member_count_v<Baz> << std::endl;
+
+    return 0;
+}();
+
+int main()
+{
+}
