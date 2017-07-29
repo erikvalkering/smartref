c77d4bd5d5e2171996a0e8cca49e52c5c8d15f7e
Erik Valkering <erikvalkering@gmail.com>
2017-07-29 19:51:20 +0200

    Added additional sanity-check

--- tests/smartref/member_types.cpp
+++ tests/smartref/member_types.cpp
@@ -2,11 +2,20 @@
 
 #include <type_traits>
 #include <vector>
+#include <list>
 
 using smartref::using_;
 
+//! A basic check to test support for std::vector<int>::value_type
 static_assert(
     std::is_same<
         using_<std::vector<int>>::value_type,
                std::vector<int> ::value_type
+    >::value);
+
+//! Check whether we are not hard-coding support for std::vector
+static_assert(
+    std::is_same<
+        using_<std::list<int>>::value_type,
+               std::list<int> ::value_type
     >::value);
