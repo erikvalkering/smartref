5e9e54f49be48e18274d32ec7417f0e020f60487
Erik Valkering <erikvalkering@gmail.com>
2017-07-29 19:03:51 +0200

    Fixed syntax of unit-test.

--- examples/smartref.cpp
+++ examples/smartref.cpp
@@ -81,9 +81,11 @@ int main()
         std::cout << x << std::endl;
     }
 
-    static_assert(std::is_same_t<
-        Property<std::vector<int>>::value_type,
-                 std::vector<int> ::value_type);
+    static_assert(
+        std::is_same<
+            Property<std::vector<int>>::value_type,
+                     std::vector<int> ::value_type
+        >::value);
 
     Property<int> x{};
     Property<int> y{};
