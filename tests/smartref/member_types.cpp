a487f1933738144bd13c4dbd8be40b90490321fc
Erik Valkering <erikvalkering@gmail.com>
2017-08-02 03:05:45 +0200

    Added an additional check for testing absence of a member-type.

--- tests/smartref/member_types.cpp
+++ tests/smartref/member_types.cpp
@@ -9,6 +9,24 @@ namespace tests {
 
 using smartref::using_;
 
+//! These tests check for absence of the value_type member-type,
+//! for using_<int>, which obviously doesn't have this member-type.
+namespace test_absence {
+
+template<typename T, typename = typename T::value_type>
+constexpr auto has_value_type(int) {return true;}
+template<typename T>
+constexpr auto has_value_type(...) {return false;}
+
+//! 1. Accessing the value_type member-type should be invalid
+//! 2. Instantiating the class *template* should work,
+//!    and should not result in an ill-formed class.
+//!    (which is the case if value_type refers to an invalid type)
+static_assert(!has_value_type<using_<int>>(0),
+              "TEST FAILED: value_type should not be defined for using_<int>!");
+
+} // namespace test_absence
+
 //! A basic check to test support for std::vector<int>::value_type
 static_assert(
     std::is_same<
