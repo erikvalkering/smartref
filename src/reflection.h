a6dd2fb64a335624ee192cd83c4aee7f69d55862
Erik Valkering <erikvalkering@gmail.com>
2017-08-26 14:48:17 +0200

    Moved template parameter inside of the REFLECTION_REFLECT_COMMON_MEMBER_FUNCTION_REFLECTOR macro.
    Apparently, it's completely an implementation detail, and therefore doesn't need to be exposed.

--- src/reflection.h
+++ src/reflection.h
@@ -141,35 +141,36 @@ constexpr auto reflected_kind_v = access::reflected_kind_v<T>;
 
 } // namespace reflection
 
-#define REFLECTION_REFLECT_COMMON_MEMBER_FUNCTION_REFLECTOR(ReflectorClassName, member, F)  \
-    class ReflectorClassName                                                                \
-        : public reflection::reflect_base<reflection::reflected_kind::member_function>      \
-    {                                                                                       \
-    private:                                                                                \
-        template<typename... ExplicitArgs, typename... Args>                                \
-        decltype(auto) indirect(Args &&... args)                                            \
-        {                                                                                   \
-            auto f = [](auto &obj, auto &&... args)                                         \
-            {                                                                               \
-                /* TODO: What if *this was an rvalue, then it should be auto &&obj */       \
-                if constexpr (sizeof...(ExplicitArgs) == 0)                                 \
-                    return obj.member(std::forward<Args>(args)...);                         \
-                else if constexpr (reflection::always_true<Args...>)                        \
-                    return obj.template member<ExplicitArgs...>(                            \
-                        std::forward<Args>(args)...);                                       \
-            };                                                                              \
-                                                                                            \
-            return F{}(*this, f, std::forward<Args>(args)...);                              \
-        }                                                                                   \
-                                                                                            \
-    public:                                                                                 \
-        template<typename... ExplicitArgs, typename... Args>                                \
-        auto member(Args &&... args)                                                        \
-            -> decltype(indirect<ExplicitArgs...>(std::forward<Args>(args)...))             \
-        {                                                                                   \
-            return indirect<ExplicitArgs...>(std::forward<Args>(args)...);                  \
-        }                                                                                   \
-    }                                                                                       \
+#define REFLECTION_REFLECT_COMMON_MEMBER_FUNCTION_REFLECTOR(ReflectorClassName, member) \
+    template<typename F>                                                                \
+    class ReflectorClassName                                                            \
+        : public reflection::reflect_base<reflection::reflected_kind::member_function>  \
+    {                                                                                   \
+    private:                                                                            \
+        template<typename... ExplicitArgs, typename... Args>                            \
+        decltype(auto) indirect(Args &&... args)                                        \
+        {                                                                               \
+            auto f = [](auto &obj, auto &&... args)                                     \
+            {                                                                           \
+                /* TODO: What if *this was an rvalue, then it should be auto &&obj */   \
+                if constexpr (sizeof...(ExplicitArgs) == 0)                             \
+                    return obj.member(std::forward<Args>(args)...);                     \
+                else if constexpr (reflection::always_true<Args...>)                    \
+                    return obj.template member<ExplicitArgs...>(                        \
+                        std::forward<Args>(args)...);                                   \
+            };                                                                          \
+                                                                                        \
+            return F{}(*this, f, std::forward<Args>(args)...);                          \
+        }                                                                               \
+                                                                                        \
+    public:                                                                             \
+        template<typename... ExplicitArgs, typename... Args>                            \
+        auto member(Args &&... args)                                                    \
+            -> decltype(indirect<ExplicitArgs...>(std::forward<Args>(args)...))         \
+        {                                                                               \
+            return indirect<ExplicitArgs...>(std::forward<Args>(args)...);              \
+        }                                                                               \
+    }                                                                                   \
 
 #define REFLECTION_REFLECT_NONINTRUSIVE(Class, member)                      \
     template<>                                                              \
@@ -189,11 +190,9 @@ constexpr auto reflected_kind_v = access::reflected_kind_v<T>;
             using detect_is_member_type = decltype(                         \
                 std::declval<typename utils::Delayed<Class, T>::member>()); \
                                                                             \
-            template<typename F>                                            \
             REFLECTION_REFLECT_COMMON_MEMBER_FUNCTION_REFLECTOR(            \
                 reflect_member_function,                                    \
-                member,                                                     \
-                F);                                                         \
+                member);                                                    \
         };                                                                  \
     };                                                                      \
                                                                             \
@@ -203,11 +202,9 @@ constexpr auto reflected_kind_v = access::reflected_kind_v<T>;
 // TODO: REFLECT currently doesn't support member-functions templates --> workaround: REFLECTION_REFLECT_NONINTRUSIVE
 // TODO: REFLECT currently doesn't support member-functions declared using 'virtual' --> workaround: REFLECTION_REFLECT_NONINTRUSIVE
 #define REFLECTION_REFLECT_INTRUSIVE_IMPL(member)                       \
-    template<typename F>                                                \
     REFLECTION_REFLECT_COMMON_MEMBER_FUNCTION_REFLECTOR(                \
         __reflect__##member,                                            \
-        member,                                                         \
-        F);                                                             \
+        member);                                                        \
                                                                         \
     template<typename Counter, class Class>                             \
     struct __type##member : Counter                                     \
