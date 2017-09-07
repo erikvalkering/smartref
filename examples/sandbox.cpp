f1baf56abfd9dae77f0fa4d8885e10d7c345d4c2
Erik Valkering <erikvalkering@gmail.com>
2017-09-07 22:57:53 +0200

    Added missing namespace

--- examples/sandbox.cpp
+++ examples/sandbox.cpp
@@ -143,19 +143,19 @@ int main()
     //     a.foobar();
     //     a.foobar(1);
     //     a.foobar(1.0);
-    //     static_assert(std::is_same<decltype(a)::some_type, GenericClassA::some_type>::value);
+    //     static_assert(std::is_same<decltype(a)::some_type, foobar::GenericClassA::some_type>::value);
 
     //     Property<foobar::GenericClassB> b;
     //     b.foobar();
     //     b.foobar(1);
-    //     static_assert(std::is_same<decltype(b)::some_type, GenericClassB::some_type>::value);
+    //     static_assert(std::is_same<decltype(b)::some_type, foobar::GenericClassB::some_type>::value);
 
     //     Property<foobar::ClassTemplate<int>> c;
     //     c.foobarbaz();
-    //     static_assert(std::is_same<decltype(c)::some_foo_type, ClassTemplate<int>::some_foo_type>::value);
+    //     static_assert(std::is_same<decltype(c)::some_foo_type, foobar::ClassTemplate<int>::some_foo_type>::value);
 
     //     Property<foobar::ClassTemplate<float>> d;
     //     d.foobarbaz();
-    //     static_assert(std::is_same<decltype(d)::some_foo_type, ClassTemplate<float>::some_foo_type>::value);
+    //     static_assert(std::is_same<decltype(d)::some_foo_type, foobar::ClassTemplate<float>::some_foo_type>::value);
     // }
 }
