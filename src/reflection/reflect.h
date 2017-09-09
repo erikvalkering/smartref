ee96d7c89ce3db8a07036e650352fc6b2da9031f
Erik Valkering <erikvalkering@gmail.com>
2017-09-09 22:24:34 +0200

    Reflections can now be compared with each other.

--- src/reflection/reflect.h
+++ src/reflection/reflect.h
@@ -3,7 +3,16 @@
 namespace reflection {
 
 template<typename Class>
-struct Reflection {};
+struct Reflection
+{
+  constexpr auto operator==(Reflection) const { return true; }
+  constexpr auto operator!=(Reflection) const { return false; }
+
+  template<typename U>
+  constexpr auto operator==(U) const { return false; }
+  template<typename U>
+  constexpr auto operator!=(U) const { return true; }
+};
 
 template<typename Class>
 constexpr auto reflect = Reflection<Class>{};
