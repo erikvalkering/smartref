c11f7cc7da35f1a6c927b125881743b3a99d24b8
Erik Valkering <erikvalkering@gmail.com>
2017-09-09 23:57:16 +0200

    Renamed macros to REFLECTABLE.

--- src/reflection.h
+++ src/reflection.h
@@ -7,16 +7,17 @@
 //! Define a set of overloads such that we can use REFLECT both inside a class,
 //! as well as outside, by dispatching on the number of arguments.
 //!
-//! REFLECTION_REFLECT(bar) -> REFLECTION_REFLECT_INTRUSIVE(bar)
-//! REFLECTION_REFLECT(Foo, bar) -> REFLECTION_REFLECT_NONINTRUSIVE(Foo, bar)
+//! REFLECTION_REFLECTABLE(bar) -> REFLECTION_REFLECTABLE_INTRUSIVE(bar)
+//! REFLECTION_REFLECTABLE(Foo, bar) -> REFLECTION_REFLECTABLE_NONINTRUSIVE(Foo, bar)
 #define REFLECTION_MAKE_OVERLOAD(_1, _2, NAME, ...) NAME
-#define REFLECTION_REFLECT(...)             \
-    REFLECTION_MAKE_OVERLOAD(               \
-        __VA_ARGS__,                        \
-        REFLECTION_REFLECT_NONINTRUSIVE,    \
-        REFLECTION_REFLECT_INTRUSIVE        \
-    )(__VA_ARGS__)                          \
+#define REFLECTION_REFLECTABLE(...)           \
+    REFLECTION_MAKE_OVERLOAD(                 \
+        __VA_ARGS__,                          \
+        REFLECTION_REFLECTABLE_NONINTRUSIVE,  \
+        REFLECTION_REFLECTABLE_INTRUSIVE      \
+    )(__VA_ARGS__)                            \
 
+// TODO: Rename to REFLECTABLE
 #ifndef REFLECTION_USE_PREFIX
-#define REFLECT(...) REFLECTION_REFLECT(__VA_ARGS__)
+#define REFLECTABLE(...) REFLECTION_REFLECTABLE(__VA_ARGS__)
 #endif
