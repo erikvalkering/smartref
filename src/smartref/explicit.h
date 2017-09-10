7104a4940a93d06f6b9529ddff2f5aeee604c61a
Erik Valkering <erikvalkering@gmail.com>
2017-09-10 16:05:41 +0200

    Restructuring project: split smartref.h into several files.

--- src/smartref/explicit.h
+++ src/smartref/explicit.h
@@ -1,14 +1,10 @@
 #pragma once
 
-#include "reflection.h"
-#include "utils.h"
+#include <utils/utils.h>
 
 #include <type_traits>
 
 namespace smartref {
-
-template<typename Delegate, class Derived = void>
-class using_;
 
 template<template<typename, typename> class Class, typename Delegate, class Derived>
 auto DerivedType(Class<Delegate, Derived> &) -> Derived;
@@ -37,59 +33,6 @@ auto &delegate(Base &base)
     {                                                                                                                                           \
         return delegate(*this).member(std::forward<T>(args)...);                                                                                \
     }                                                                                                                                           \
-
-template<typename Delegate, class Derived>
-struct Forwarder
-{
-    // TODO: Maybe not access this directly,
-    //       but using a helper function.
-    using Class = Delegate;
-
-    template<typename Self, typename F, typename... Args>
-    auto operator()(Self &self, F f, Args &&... args)
-    {
-        static_assert(std::is_base_of<Self, Derived>::value);
-
-        //! Downcast to the derived class
-        auto &derived = static_cast<Derived &>(self);
-
-        //! Now invoke the conversion operator
-        auto &delegate = static_cast<Delegate &>(derived);
-
-        return f(delegate, std::forward<decltype(args)>(args)...);
-    }
-};
-
-template<typename Delegate, class Derived>
-struct Members {};
-
-template<typename Delegate, class Derived, typename Reflection>
-using using_member_t = decltype(
-    reflection::reify(
-        Reflection{},
-        Forwarder<Delegate, Derived>{}
-    )
-);
-
-template<typename tag, typename Delegate, class Derived, typename Members>
-struct ReflectedMembersImpl;
-
-template<typename tag, typename Delegate, class Derived, typename... Reflections>
-struct ReflectedMembersImpl<tag, Delegate, Derived, std::tuple<Reflections...>>
-    : using_member_t<Delegate, Derived, Reflections>...
-{
-};
-
-template<
-    typename Delegate,
-    class Derived,
-    class ReflectionClass = Delegate>
-using ReflectedMembers = ReflectedMembersImpl<
-    ReflectionClass,
-    Delegate,
-    Derived,
-    decltype(members(reflection::reflect<ReflectionClass>))
->;
 
 template<typename MemberTypeTag, typename Delegate, typename = void>
 struct member_type_introducer
@@ -133,30 +76,6 @@ struct STL : USING_MEMBER_TYPE(Delegate, value_type)
 };
 
 template<typename Delegate, class Derived>
-struct using_base
-{
-    operator Delegate &()
-    {
-        auto &derived = static_cast<Derived &>(*this);
-        return static_cast<Delegate &>(derived);
-    }
-};
-
-template<typename Delegate>
-struct using_base<Delegate, void>
-{
-    // TODO: Add some checks to see whether this doesn't add too much overhead
-    //       for this specific Delegate type.
-    virtual operator Delegate &() = 0;
-};
-
-template<typename Delegate, class Derived>
-class using_ : public using_base<Delegate, Derived>
-             , public Members<Delegate, using_<Delegate, Derived>>
-             , public STL<Delegate, using_<Delegate, Derived>>
-             , public ReflectedMembers<Delegate, using_<Delegate, Derived>>
-             , public ReflectedMembers<Delegate, using_<Delegate, Derived>, utils::Delayed<reflection::auto_, Delegate>>
-{
-};
+struct Members {};
 
 } // namespace smartref
