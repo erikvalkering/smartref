7fb18bf2a67466e19c61c73e90c75021cca4d213
Erik Valkering <erikvalkering@gmail.com>
2017-09-29 00:20:03 -0700

    Added tests for operator>>= and <<=

--- tests/reflection/reflect_operators.cpp
+++ tests/reflection/reflect_operators.cpp
@@ -1,12 +1,14 @@
 #include <reflection/reflection.h>
 
-REFLECTABLE(auto, operator[], member_function);
-REFLECTABLE(auto, operator=,  member_function);
-REFLECTABLE(auto, operator==, member_function);
-REFLECTABLE(auto, operator!=, member_function);
-REFLECTABLE(auto, operator<,  member_function);
-REFLECTABLE(auto, operator<=, member_function);
-REFLECTABLE(auto, operator>,  member_function);
-REFLECTABLE(auto, operator>=, member_function);
-REFLECTABLE(auto, operator>>, member_function);
-REFLECTABLE(auto, operator<<, member_function);
+REFLECTABLE(auto, operator[],  member_function);
+REFLECTABLE(auto, operator=,   member_function);
+REFLECTABLE(auto, operator==,  member_function);
+REFLECTABLE(auto, operator!=,  member_function);
+REFLECTABLE(auto, operator<,   member_function);
+REFLECTABLE(auto, operator<=,  member_function);
+REFLECTABLE(auto, operator>,   member_function);
+REFLECTABLE(auto, operator>=,  member_function);
+REFLECTABLE(auto, operator>>,  member_function);
+REFLECTABLE(auto, operator<<,  member_function);
+REFLECTABLE(auto, operator>>=, member_function);
+REFLECTABLE(auto, operator<<=, member_function);
