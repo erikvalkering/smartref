3f46167c56cb62e08ec633d52d3f58c7def474c7
Erik Valkering <erikvalkering@gmail.com>
2018-01-02 19:06:15 +0100

    Reformatted the trailing return type.

--- src/smartref/implicit.h
+++ src/smartref/implicit.h
@@ -30,7 +30,8 @@ struct Forwarder
 
     template<typename Self, typename F, typename Arg>
     // decltype(auto) operator()(Self &self, F f, Args &&... args)
-    auto operator()(Self &self, F f, Arg arg) -> decltype(f(delegate(self), arg))
+    auto operator()(Self &self, F f, Arg arg)
+        -> decltype(f(delegate(self), arg))
     {
 // int x = 0;
         // return f(x, std::forward<decltype(args)>(args)...);
