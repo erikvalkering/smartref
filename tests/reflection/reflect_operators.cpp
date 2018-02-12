1996100b1c188ad2d50ff6d311a743f16251799b
Erik Valkering <erikvalkering@gmail.com>
2018-02-12 23:39:42 +0100

    Added assignment operator.

--- tests/reflection/reflect_operators.cpp
+++ tests/reflection/reflect_operators.cpp
@@ -23,7 +23,6 @@
 // REFLECTABLE(auto, operator<<,   member_function);
 // REFLECTABLE(auto, operator<<=,  member_function);
 // REFLECTABLE(auto, operator<=,   member_function);
-// REFLECTABLE(auto, operator=,    member_function);
 // REFLECTABLE(auto, operator==,   member_function);
 // REFLECTABLE(auto, operator>,    member_function);
 // REFLECTABLE(auto, operator>=,   member_function);
@@ -39,6 +38,8 @@
 // REFLECTION_REFLECTABLE_NONINTRUSIVE_UNARY_OPERATOR(auto, operator!,   member_function);
 // REFLECTION_REFLECTABLE_NONINTRUSIVE_UNARY_OPERATOR(auto, operator->,  member_function);
 // REFLECTION_REFLECTABLE_NONINTRUSIVE_UNARY_OPERATOR(auto, operator~,   member_function);
+
+REFLECTION_REFLECTABLE_NONINTRUSIVE_ASSIGNMENT_OPERATOR(auto, operator=, member_function);
 
 // TODO: operator,
 // #define REFLECTION_OPERATOR_COMMA operator,
