e0ed15500ae17f0ff94e1c3d7a4f8fe7b7ea4392
Erik Valkering <erikvalkering@gmail.com>
2018-02-13 18:03:06 +0100

    Added TODO

--- src/smartref/smartref.h
+++ src/smartref/smartref.h
@@ -70,6 +70,8 @@ public:
 // TODO: this hook cannot be overridden if the using_<T> syntax is used,
 //       which requires a runtime double dispatch mechanism.
 // TODO: Fix ExplicitArgs
+// TODO: Reflection is not the actual member reflection, but the reflector
+//       (i.e. the class from which we inherit the member-function)
 template<typename Reflection, typename Delegate, typename Derived, typename Args>
 auto on_call(Reflection reflection, using_<Delegate, Derived> &self, Args args)
   -> decltype(call(reflection, static_cast<Delegate &>(self), std::forward<Args>(args)))
