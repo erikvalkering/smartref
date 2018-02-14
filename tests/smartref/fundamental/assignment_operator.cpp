f4ad289c8f012af1ce3fc8534eaf97ef011be57c
Erik Valkering <erikvalkering@gmail.com>
2018-02-15 00:22:26 +0100

    Moved the assignment operator unit tests into a separate file.

---
+++ tests/smartref/fundamental/assignment_operator.cpp
@@ -0,0 +1,137 @@
+#include <reflection/reflection.h>
+#include <smartref/smartref.h>
+
+#include <cassert>
+#include <string>
+
+namespace tests_assignment {
+
+template<typename T>
+struct Ref : smartref::using_<T>
+{
+  T ref;
+
+  static auto &counter()
+  {
+    static auto count = 0;
+    return count;
+  }
+
+  operator T &()
+  {
+    ++counter();
+    return ref;
+  }
+
+  Ref(T arg) : ref{arg} {}
+
+  using smartref::using_<T>::operator=;
+
+  Ref() = default;
+
+  Ref(const Ref &) = default;
+  Ref &operator=(const Ref &) = default;
+  Ref(Ref &&) = default;
+  Ref &operator=(Ref &&) = default;
+};
+
+template<typename T, typename Delegate>
+auto test = []{
+  //! Uninitialized construction
+  T a;
+
+  //! Default construction
+  T b{};
+  T c = {};
+
+  //! Copy construction
+  T d{a};
+  T e = a;
+  T f = {a};
+  auto g{a};
+  auto h = a;
+  auto i = {a};
+
+  //! Move construction
+  T j{std::move(a)};
+  T k = std::move(a);
+  T l = {std::move(a)};
+  auto m{T{}};
+  auto n = T{};
+  auto o = {T{}};
+
+  //! Delegate type copy construction
+  auto delegate = Delegate{};
+  T p{delegate};
+  T q = delegate;
+  T r = {delegate};
+  auto s{T{delegate}};
+  auto t = T{delegate};
+  auto u = {T{delegate}};
+
+  //! Delegate type move construction
+  T v{std::move(delegate)};
+  T w = std::move(delegate);
+  T x = {std::move(delegate)};
+  auto y{T{std::move(delegate)}};
+  auto z = T{std::move(delegate)};
+  auto A = {T{std::move(delegate)}};
+
+  // TODO: Check whether semantics are right (e.g. (a = b)++ changes a, instead of a copy).
+  //! Copy assignment
+  a = b;
+  a = b = c;
+  a = (b = c);
+  (a = b) = c;
+  (a = b) = delegate; // TODO: also add similar checks for other tests.
+
+  //! Move assignment
+  a = std::move(b);
+  a = b = std::move(c);
+  a = (b = std::move(c));
+  (a = b) = std::move(c);
+
+  //! Delegate type copy assignment
+  a = delegate;
+  a = b = delegate;
+  a = (b = delegate);
+  (a = b) = delegate;
+
+  //! Delegate type move assignment
+  a = std::move(delegate);
+  a = b = std::move(delegate);
+  a = (b = std::move(delegate));
+  (a = b) = std::move(delegate);
+
+  return 0;
+};
+
+auto test_int = test<int, int>();
+
+auto test_ref = []{
+  Ref<int>::counter() = 0;
+  test<Ref<int>, int>();
+  printf("counter: %d\n", Ref<int>::counter());
+  assert(Ref<int>::counter() == 34);
+
+  Ref<float>::counter() = 0;
+  test<Ref<float>, float>();
+  assert(Ref<float>::counter() == 34);
+
+  Ref<bool>::counter() = 0;
+  test<Ref<bool>, bool>();
+  assert(Ref<bool>::counter() == 34);
+
+  Ref<std::string>::counter() = 0;
+  test<Ref<std::string>, std::string>();
+  assert(Ref<std::string>::counter() == 34);
+
+  struct Foo {};
+  Ref<Foo>::counter() = 0;
+  test<Ref<Foo>, Foo>();
+  assert(Ref<Foo>::counter() == 34);
+
+  return 0;
+}();
+
+} // namespace tests_assignment
