35a972ab03220d502efb1ead50f745bf529c9085
Erik Valkering <erikvalkering@gmail.com>
2018-03-20 18:20:39 +0100

    Renamed parameter packs.

--- src/reflection/reflect_members.h
+++ src/reflection/reflect_members.h
@@ -13,14 +13,14 @@ namespace detail {
 template<typename Class, typename index_pack_non_intrusive, typename index_pack_non_intrusive_auto>
 struct Members;
 
-template<typename Class, size_t... indices_non_intrusive, size_t... indices_nonintrusive_auto>
-struct Members<Class, std::index_sequence<indices_non_intrusive...>, std::index_sequence<indices_nonintrusive_auto...>>
+template<typename Class, size_t... indices, size_t... indices_auto>
+struct Members<Class, std::index_sequence<indices...>, std::index_sequence<indices_auto...>>
 {
     constexpr static auto _()
     {
         return utils::type_list<
-            reflected_member_t<Class, indices_non_intrusive>...,
-            reflected_member_t<utils::Delayed<auto_, Class>, indices_nonintrusive_auto>...
+            reflected_member_t<Class, indices>...,
+            reflected_member_t<utils::Delayed<auto_, Class>, indices_auto>...
         >{};
     }
 };
