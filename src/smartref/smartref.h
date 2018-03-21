763cf22cbe405af8a3195ba3335dc0a78928294b
Erik Valkering <erikvalkering@gmail.com>
2018-03-21 23:51:54 +0100

    Removed obsolete TODO.

--- src/smartref/smartref.h
+++ src/smartref/smartref.h
@@ -71,7 +71,6 @@ auto delegate(using_<Delegate, Derived> &base)
 // TODO: -cmaster Reflection should be named Reflector (better: rename everything)
 // TODO: -cmaster Instead of passing the reflector, pass a Reflection, such that we can also reify that directly
 // TODO: -cmaster args should use forwarding references (unit test this!)
-// TODO: -cmaster Maybe it's better to make Reflection *only* a template parameter
 // TODO: -cmaster Document "Incomplete type support" (e.g. perfect pimpl)
 template<typename Reflection, typename Delegate, typename Derived, typename... ExplicitArgs, typename... Args>
 auto on_call(Reflection &reflection, using_<Delegate, Derived> &self, utils::type_list<ExplicitArgs...> explicitArgs, Args... args)
