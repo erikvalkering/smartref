8a59171605fbb7ef912595a2aeee4ee314855573
Erik Valkering <erikvalkering@gmail.com>
2018-04-02 21:13:08 +0100

    Renamed directory containing header files to 'include'.
    - This way, it becomes more clear that it is a header-only library.
    - It is the default in conan, which makes the conanfile.py simpler.

--- conanfile.py
+++ conanfile.py
@@ -4,8 +4,8 @@ from conans import ConanFile
 class HelloConan(ConanFile):
     name = "smartref"
     version = "master"
-    exports_sources = "src/*"
+    exports_sources = "include/*"
     no_copy_source = True
 
     def package(self):
-        self.copy("*.h", dst='include', src='src')
+        self.copy("*.h")
