1e90f3c968d1acafd041aa41038a41441159baa2
Erik Valkering <erikvalkering@gmail.com>
2018-04-02 21:25:03 +0100

    Added conan package test.

---
+++ test_package/conanfile.py
@@ -0,0 +1,11 @@
+from conans import ConanFile, CMake
+
+
+class TestPackage(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "cmake"
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
