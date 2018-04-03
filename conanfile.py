40d13a86ee1e003fcd2482403f5ba751ec3aa7e6
Erik Valkering <erikvalkering@gmail.com>
2018-04-03 12:49:52 +0200

    Added missing attributes to suppress warnings.

--- conanfile.py
+++ conanfile.py
@@ -4,6 +4,10 @@ from conans import ConanFile
 class HelloConan(ConanFile):
     name = "smartref"
     version = "master"
+    url = 'https://github.com/erikvalkering/smartref'
+    license = 'mit'
+    description = 'A modern header-only zero-overhead library ' + \
+                  'for creating smart references'
     exports_sources = "include/*"
     no_copy_source = True
 
