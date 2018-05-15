3353eaef481d6979d95e8108b9800ce8123cb9c0
Erik Valkering <erikvalkering@gmail.com>
2018-05-15 18:36:25 +0200

    Suffixed Empty/NonConst/Const/Mixed classes with 'Class'.

--- tests/smartref/member_functions.cpp
+++ tests/smartref/member_functions.cpp
@@ -14,19 +14,19 @@ constexpr auto has_member(int) -> decltype(std::declval<T>().member(), bool{}) {
 template<typename T>
 constexpr auto has_member(...) {return false;}
 
-struct Empty {};
+struct EmptyClass {};
 
-struct NonConst
+struct NonConstClass
 {
   void member() {}
 };
 
-struct Const
+struct ConstClass
 {
   void member() const {}
 };
 
-struct Mixed
+struct MixedClass
 {
   void member() {}
   void member() const {}
@@ -36,46 +36,46 @@ struct Mixed
 // non-const member functions //
 ////////////////////////////////
 
-static_assert(!has_member<Empty>(0),
-              "TEST FAILED: Empty seems to have a member-function!");
+static_assert(!has_member<EmptyClass>(0),
+              "TEST FAILED: EmptyClass seems to have a member-function!");
 
-static_assert(!has_member<using_<Empty>>(0),
-              "TEST FAILED: using_<Empty> seems to have a member-function!");
+static_assert(!has_member<using_<EmptyClass>>(0),
+              "TEST FAILED: using_<EmptyClass> seems to have a member-function!");
 
-static_assert(has_member<NonConst>(0),
-              "TEST FAILED: NonConst doesn't seem to have a member-function!");
+static_assert(has_member<NonConstClass>(0),
+              "TEST FAILED: NonConstClass doesn't seem to have a member-function!");
 
-static_assert(has_member<using_<NonConst>>(0),
-              "TEST FAILED: using_<NonConst> doesn't seem to have a member-function!");
+static_assert(has_member<using_<NonConstClass>>(0),
+              "TEST FAILED: using_<NonConstClass> doesn't seem to have a member-function!");
 
-static_assert(has_member<Const>(0),
-              "TEST FAILED: Const doesn't seem to have a member-function!");
+static_assert(has_member<ConstClass>(0),
+              "TEST FAILED: ConstClass doesn't seem to have a member-function!");
 
 // TODO: This should currently fail
-static_assert(has_member<using_<Const>>(0),
-              "TEST FAILED: using_<Const> doesn't seem to have a member-function!");
+static_assert(has_member<using_<ConstClass>>(0),
+              "TEST FAILED: using_<ConstClass> doesn't seem to have a member-function!");
 
-static_assert(has_member<Mixed>(0),
-              "TEST FAILED: Mixed doesn't seem to have a member-function!");
+static_assert(has_member<MixedClass>(0),
+              "TEST FAILED: MixedClass doesn't seem to have a member-function!");
 
-static_assert(has_member<using_<Mixed>>(0),
-              "TEST FAILED: using_<Mixed> doesn't seem to have a member-function!");
+static_assert(has_member<using_<MixedClass>>(0),
+              "TEST FAILED: using_<MixedClass> doesn't seem to have a member-function!");
 
 ////////////////////////////
 // const member functions //
 ////////////////////////////
 
-static_assert(!has_member<const Empty>(0),
-              "TEST FAILED: const Empty seems to have a const member-function!");
+static_assert(!has_member<const EmptyClass>(0),
+              "TEST FAILED: const EmptyClass seems to have a const member-function!");
 
-static_assert(!has_member<const using_<Empty>>(0),
-              "TEST FAILED: const using_<Empty> seems to have a const member-function!");
+static_assert(!has_member<const using_<EmptyClass>>(0),
+              "TEST FAILED: const using_<EmptyClass> seems to have a const member-function!");
 
-static_assert(!has_member<const NonConst>(0),
-              "TEST FAILED: const NonConst seems to have a const member-function!");
+static_assert(!has_member<const NonConstClass>(0),
+              "TEST FAILED: const NonConstClass seems to have a const member-function!");
 
-static_assert(!has_member<const using_<NonConst>>(0),
-              "TEST FAILED: const using_<NonConst> seem to have a const member-function!");
+static_assert(!has_member<const using_<NonConstClass>>(0),
+              "TEST FAILED: const using_<NonConstClass> seem to have a const member-function!");
 
 } // namespace test_existence
 } // namespace tests
