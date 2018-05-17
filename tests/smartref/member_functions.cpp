a039f0c106ad9c7d480fba311c0b281e10eee390
Erik Valkering <erikvalkering@gmail.com>
2018-05-17 22:44:53 +0200

    Removed obviously passing tests.

--- tests/smartref/member_functions.cpp
+++ tests/smartref/member_functions.cpp
@@ -42,29 +42,17 @@ struct MixedMemberClass
 // non-const member functions //
 ////////////////////////////////
 
-static_assert(!has_member<EmptyClass>(0),
-              "TEST FAILED: EmptyClass seems to have a member-function!");
-
 static_assert(!has_member<using_<EmptyClass>>(0),
               "TEST FAILED: using_<EmptyClass> seems to have a member-function!");
 
-static_assert(has_member<NonConstMemberClass>(0),
-              "TEST FAILED: NonConstMemberClass doesn't seem to have a member-function!");
-
 static_assert(has_member<using_<NonConstMemberClass>>(0),
               "TEST FAILED: using_<NonConstMemberClass> doesn't seem to have a member-function!");
-
-static_assert(has_member<ConstMemberClass>(0),
-              "TEST FAILED: ConstMemberClass doesn't seem to have a member-function!");
 
 static_assert(has_member<using_<ConstMemberClass>>(0),
               "TEST FAILED: using_<ConstMemberClass> doesn't seem to have a member-function!");
 
 static_assert(is_const_member_function(&using_<ConstMemberClass>::member<>),
               "TEST FAILED: using_<ConstMemberClass> doesn't seem to have a const member-function!");
-
-static_assert(has_member<MixedMemberClass>(0),
-              "TEST FAILED: MixedMemberClass doesn't seem to have a member-function!");
 
 static_assert(has_member<using_<MixedMemberClass>>(0),
               "TEST FAILED: using_<MixedMemberClass> doesn't seem to have a member-function!");
@@ -73,14 +61,8 @@ static_assert(has_member<using_<MixedMemberClass>>(0),
 // const member functions //
 ////////////////////////////
 
-static_assert(!has_member<const EmptyClass>(0),
-              "TEST FAILED: const EmptyClass seems to have a const member-function!");
-
 static_assert(!has_member<const using_<EmptyClass>>(0),
               "TEST FAILED: const using_<EmptyClass> seems to have a const member-function!");
-
-static_assert(!has_member<const NonConstMemberClass>(0),
-              "TEST FAILED: const NonConstMemberClass seems to have a const member-function!");
 
 static_assert(!has_member<const using_<NonConstMemberClass>>(0),
               "TEST FAILED: const using_<NonConstMemberClass> seem to have a const member-function!");
