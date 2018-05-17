f28ec3ce65560790ef7ecfd29d36f583f130b8d7
Erik Valkering <erikvalkering@gmail.com>
2018-05-17 22:17:14 +0200

    Added a check to determine whether the generated member function is const.
    This currently fails.

--- tests/smartref/member_functions.cpp
+++ tests/smartref/member_functions.cpp
@@ -1,5 +1,7 @@
 #include <smartref/smartref.h>
 
+#include <type_traits>
+
 REFLECTABLE(member);
 
 namespace tests {
@@ -13,6 +15,10 @@ template<typename T>
 constexpr auto has_member(int) -> decltype(std::declval<T>().member(), bool{}) {return true;}
 template<typename T>
 constexpr auto has_member(...) {return false;}
+
+template<typename Class>
+constexpr auto is_const_member_function(void (Class::*)() const) { return true;  }
+constexpr auto is_const_member_function(...)                     { return false; }
 
 struct EmptyClass {};
 
@@ -51,10 +57,11 @@ static_assert(has_member<using_<NonConstMemberClass>>(0),
 static_assert(has_member<ConstMemberClass>(0),
               "TEST FAILED: ConstMemberClass doesn't seem to have a member-function!");
 
-// TODO: This reifies a non-const member-function that delegates to the const member-function
-//       It should reify a const member-function
 static_assert(has_member<using_<ConstMemberClass>>(0),
               "TEST FAILED: using_<ConstMemberClass> doesn't seem to have a member-function!");
+
+static_assert(is_const_member_function(&using_<ConstMemberClass>::member<>),
+              "TEST FAILED: using_<ConstMemberClass> doesn't seem to have a const member-function!");
 
 static_assert(has_member<MixedMemberClass>(0),
               "TEST FAILED: MixedMemberClass doesn't seem to have a member-function!");
