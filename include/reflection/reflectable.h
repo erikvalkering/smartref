f24c673bbfb985b67962fdeaa2565fca9ba317ef
Erik Valkering <erikvalkering@gmail.com>
2018-06-05 18:34:20 +0200

    Added (currently empty) support for free functions to the REFLECTABLE macro.

--- include/reflection/reflectable.h
+++ include/reflection/reflectable.h
@@ -45,39 +45,46 @@ constexpr auto reflected_member_count_v = reflected_member_count<reflected_membe
 
 // TODO: -cmaster Have a quick look whether we can simplify these macros.
 // TODO: -cmaster We can replace the using type = ... with a simple boolean, such that we can inline the reflectors
-#define REFLECTION_REFLECTABLE_UNIFIED(Class, member, MEMBER_TYPE_REFLECTOR, MEMBER_FUNCTION_REFLECTOR) \
-  constexpr auto CONCAT(reflection, __LINE__) =                                                         \
-    REFLECTION_REFLECT_AUTO(Class);                                                                     \
-                                                                                                        \
-  using CONCAT(CLASS, __LINE__) =                                                                       \
-    decltype(reify(CONCAT(reflection, __LINE__)));                                                      \
-                                                                                                        \
-  template<>                                                                                            \
-  struct reflection::reflected_member<                                                                  \
-    CONCAT(CLASS, __LINE__),                                                                            \
-    CURRENT_COUNTER(CONCAT(CLASS, __LINE__))                                                            \
-  >                                                                                                     \
-  {                                                                                                     \
-    using type = struct                                                                                 \
-    {                                                                                                   \
-      MEMBER_TYPE_REFLECTOR(                                                                            \
-        reflector_member_type,                                                                          \
-        member);                                                                                        \
-                                                                                                        \
-      MEMBER_FUNCTION_REFLECTOR(                                                                        \
-        reflector_member_function,                                                                      \
-        member);                                                                                        \
-    };                                                                                                  \
-  };                                                                                                    \
-                                                                                                        \
-  INC_COUNTER(CONCAT(CLASS, __LINE__))                                                                  \
+#define REFLECTION_REFLECTABLE_UNIFIED(Class, member,                         \
+  MEMBER_TYPE_REFLECTOR, MEMBER_FUNCTION_REFLECTOR, FREE_FUNCTION_REFLECTOR   \
+)                                                                             \
+  constexpr auto CONCAT(reflection, __LINE__) =                               \
+    REFLECTION_REFLECT_AUTO(Class);                                           \
+                                                                              \
+  using CONCAT(CLASS, __LINE__) =                                             \
+    decltype(reify(CONCAT(reflection, __LINE__)));                            \
+                                                                              \
+  template<>                                                                  \
+  struct reflection::reflected_member<                                        \
+    CONCAT(CLASS, __LINE__),                                                  \
+    CURRENT_COUNTER(CONCAT(CLASS, __LINE__))                                  \
+  >                                                                           \
+  {                                                                           \
+    using type = struct                                                       \
+    {                                                                         \
+      MEMBER_TYPE_REFLECTOR(                                                  \
+        reflector_member_type,                                                \
+        member);                                                              \
+                                                                              \
+      MEMBER_FUNCTION_REFLECTOR(                                              \
+        reflector_member_function,                                            \
+        member);                                                              \
+                                                                              \
+      FREE_FUNCTION_REFLECTOR(                                                \
+        reflector_free_function,                                              \
+        member);                                                              \
+    };                                                                        \
+  };                                                                          \
+                                                                              \
+  INC_COUNTER(CONCAT(CLASS, __LINE__))                                        \
 
 #define REFLECTION_REFLECTABLE2(Class, member)            \
   REFLECTION_REFLECTABLE_UNIFIED(                         \
     Class,                                                \
     member,                                               \
     REFLECTION_REFLECTABLE_ADD_MEMBER_TYPE_REFLECTOR,     \
-    REFLECTION_REFLECTABLE_ADD_MEMBER_FUNCTION_REFLECTOR  \
+    REFLECTION_REFLECTABLE_ADD_MEMBER_FUNCTION_REFLECTOR, \
+    REFLECTION_REFLECTABLE_ADD_EMPTY_REFLECTOR            \
   )                                                       \
 
 #define REFLECTION_REFLECTABLE1(member) \
@@ -88,19 +95,21 @@ constexpr auto reflected_member_count_v = reflected_member_count<reflected_membe
     Class,                                                \
     member,                                               \
     REFLECTION_REFLECTABLE_ADD_EMPTY_REFLECTOR,           \
-    REFLECTION_REFLECTABLE_ADD_MEMBER_FUNCTION_REFLECTOR  \
+    REFLECTION_REFLECTABLE_ADD_MEMBER_FUNCTION_REFLECTOR, \
+    REFLECTION_REFLECTABLE_ADD_EMPTY_REFLECTOR            \
   )                                                       \
 
 #define REFLECTION_REFLECTABLE_OPERATOR1(member)  \
   REFLECTION_REFLECTABLE_OPERATOR2(auto, member)  \
 
-#define REFLECTION_REFLECTABLE_OPERATOR_UNARY2(Class, member)         \
-  REFLECTION_REFLECTABLE_UNIFIED(                                     \
-    Class,                                                            \
-    member,                                                           \
-    REFLECTION_REFLECTABLE_ADD_EMPTY_REFLECTOR,                       \
-    REFLECTION_REFLECTABLE_ADD_MEMBER_FUNCTION_REFLECTOR_NON_TEMPLATE \
-  )                                                                   \
+#define REFLECTION_REFLECTABLE_OPERATOR_UNARY2(Class, member)           \
+  REFLECTION_REFLECTABLE_UNIFIED(                                       \
+    Class,                                                              \
+    member,                                                             \
+    REFLECTION_REFLECTABLE_ADD_EMPTY_REFLECTOR,                         \
+    REFLECTION_REFLECTABLE_ADD_MEMBER_FUNCTION_REFLECTOR_NON_TEMPLATE,  \
+    REFLECTION_REFLECTABLE_ADD_EMPTY_REFLECTOR                          \
+  )                                                                     \
 
 #define REFLECTION_REFLECTABLE_OPERATOR_UNARY1(member)  \
   REFLECTION_REFLECTABLE_OPERATOR_UNARY2(auto, member)  \
@@ -110,7 +119,8 @@ constexpr auto reflected_member_count_v = reflected_member_count<reflected_membe
     Class,                                                                    \
     member,                                                                   \
     REFLECTION_REFLECTABLE_ADD_EMPTY_REFLECTOR,                               \
-    REFLECTION_REFLECTABLE_ADD_MEMBER_FUNCTION_REFLECTOR_ASSIGNMENT_OPERATOR  \
+    REFLECTION_REFLECTABLE_ADD_MEMBER_FUNCTION_REFLECTOR_ASSIGNMENT_OPERATOR, \
+    REFLECTION_REFLECTABLE_ADD_EMPTY_REFLECTOR                                \
   )                                                                           \
 
 #define REFLECTION_REFLECTABLE_OPERATOR_ASSIGNMENT1(member) \
