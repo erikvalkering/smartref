7dee8a440c4e32ebae47d4f5885c08984f1fc7cc
Erik Valkering <erikvalkering@gmail.com>
2018-08-06 20:59:08 +0200

    Added more stdlibs to the beginning of sandbox.cpp for ease of debugging.

--- examples/sandbox.cpp
+++ examples/sandbox.cpp
@@ -1,8 +1,14 @@
 //! These includes are put here, such that in the preprocessed file
 //! they end up first, making debugging a bit more easy.
-#include <typeinfo>
+#include <cassert>
 #include <cstddef>
+#include <functional>
 #include <iostream>
+#include <list>
+#include <string>
+#include <type_traits>
+#include <typeinfo>
+#include <utility>
 #include <vector>
 
 #include "foobar.h"
