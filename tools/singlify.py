0aac513f8ece5adbe7eb79cac196604bfa934b3b
Erik Valkering <erikvalkering@gmail.com>
2018-08-23 09:05:27 +0200

    Non-recursively parsing a header file.

--- tools/singlify.py
+++ tools/singlify.py
@@ -1,6 +1,23 @@
-def main():
-    pass
+import sys
 
+
+def singlify(filename):
+    with open(filename) as f:
+        for line in f.readlines():
+            # Strip off newline
+            line = line[:-1]
+
+            if '#include' in line:
+                # TODO
+                continue
+
+            yield line
+
+
+def main():
+    filename = sys.argv[1]
+    for line in singlify(filename):
+        print(line)
 
 if __name__ == '__main__':
     main()
