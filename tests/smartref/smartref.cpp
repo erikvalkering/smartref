51aa652ee783e6b8a4230d7738e7d0b7b91c2afa
Erik Valkering <erikvalkering@gmail.com>
2018-09-02 18:01:32 +0200

    Added some extra tests for DegelateType.

--- tests/smartref/smartref.cpp
+++ tests/smartref/smartref.cpp
@@ -1,1 +1,16 @@
 #include <smartref/smartref.h>
+#include <reflection/reflect.h>
+#include <utils/utils.h>
+
+using namespace std;
+using namespace smartref;
+using namespace reflection;
+
+template<typename T>
+struct RefVirtual : using_<T> {};
+
+template<typename T>
+struct RefCRTP : using_<T, RefCRTP<T>> {};
+
+static_assert(reflect<DelegateType<decltype(declval<RefVirtual<int> &>())>> == reflect<int>);
+static_assert(reflect<DelegateType<decltype(declval<RefCRTP<int> &>())>> == reflect<int>);
