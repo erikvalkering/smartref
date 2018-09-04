3d8a3bb9c460c0cab35b546656268953da1d3f82
Erik Valkering <erikvalkering@gmail.com>
2018-09-04 22:09:16 +0200

    Added new example: unified call syntax.
    TODO: the actual implementation.
    
    - Added project to CMakeLists.txt
    - Added skeleton for example
    - Added (unrelated) experiment for &auto::foo emulation.

---
+++ examples/ufcs.cpp
@@ -0,0 +1,65 @@
+/*********************************************/
+/* Example: unified call syntax              */
+/*********************************************/
+#include <vector>
+
+#include <smartref/smartref.h>
+
+namespace magic {
+
+auto wand = [](auto x) { return x; };
+
+} // namespace magic
+
+using namespace std;
+
+// Algorithms
+auto sorted    = [](auto container)                 { return container; };
+auto unique    = [](auto container)                 { return container; };
+auto filtered  = [](auto container, auto predicate) { return container; };
+auto transform = [](auto container, auto operation) { return container; };
+auto sum       = [](auto container)                 { return 42;        };
+
+// Functors
+auto is_even = [](auto x) { return x % 2 == 0; };
+auto squared = [](auto x) { return x * x;      };
+
+auto $ = magic::wand;
+
+int main()
+{
+  auto v = vector<int>{1, 2, 2, 1, 3, 2, 4, 5};
+  // auto v = vector{1, 2, 2, 1, 3, 2, 4, 5};
+
+  {
+    // Variant 1a: single-line, read from inside outwards (continuously looking left *and* right)
+    auto s = sum(transform(filtered(unique(sorted(v)), is_even), squared));
+  }
+
+  {
+    // Variant 1b: multi-line, read from inside outwards (continuously looking left *and* right)
+    auto s = sum(
+               transform(
+                 filtered(
+                   unique(sorted(v)),
+                   is_even
+                 ),
+                 squared
+               )
+             );
+  }
+
+  {
+    // Variant 2a: single-line, read from left to right
+    auto s = $(v).sorted().unique().filtered(is_even).transform(squared).sum();
+  }
+
+  {
+    // Variant 2b: multi-line, read from top to bottom
+    auto s = $(v).sorted()
+                 .unique()
+                 .filtered(is_even)
+                 .transform(squared)
+                 .sum();
+  }
+}
